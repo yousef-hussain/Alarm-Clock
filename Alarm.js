cb6b2adbab0093c2624553c1f6d0073c1afbc7dc
yousef-hussain <yousef123hussain@gmail.com>
2022-06-14 09:15:53 +0300

    Alarm project

---
+++ Alarm.js
@@ -0,0 +1,50 @@
+const display = document.getElementById('clock');
+const audio = new Audio('audio.mp3');
+audio.loop = true;
+let alarmTime = null;
+let alarmTimeout = null;
+
+function updateTime() {
+    const date = new Date();
+    const hour = formatTime(date.getHours());
+    const minutes = formatTime(date.getMinutes());
+    const seconds = formatTime(date.getSeconds());
+
+    display.innerText= `${hour} : ${minutes} : ${seconds}`;
+}
+function formatTime(time) {
+    if(time < 10) {
+        return '0' + time;
+    }
+    return time;
+}
+
+function setAlarmTime (value) {
+    alarmTime = value;
+    console.log(alarmTime);
+}
+
+
+function setAlarm() {
+    if (alarmTime) {
+        const current = new Date();
+        const timeToAlarm = new Date(alarmTime);
+        
+        if (timeToAlarm > current) {
+            const timeout = timeToAlarm.getTime() - current.getTime();
+            alarmTimeout = setTimeout(() => audio.play(), timeout);
+            alert(`Alarm set of ${timeToAlarm.getHours()} : ${timeToAlarm.getMinutes()} : ${timeToAlarm.getSeconds()}`);
+            
+        }
+    }
+}
+
+function clearAlarm () {
+    audio.pause();
+    if (alarmTimeout) {
+        clearTimeout (alarmTimeout);
+        alert('Alarm cleared');
+    }
+}
+
+setInterval(updateTime, 1000);
